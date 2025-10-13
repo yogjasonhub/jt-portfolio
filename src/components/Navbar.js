3db4a6e87c7f9f4fa6c56127ec468ccef8b56aa7
Jason Thao <jasonthaoLjt@gmail.com>
2025-10-13 09:22:33 -0500

    updated navbar component
    updated navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
     `}>
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 flex items-center">
         {/* Logo on the left */}
-        <div className="flex-shrink-0 -ml-2 sm:ml-0">
+        <div className="flex-shrink-0 ml-2 sm:ml-4">
           <Link
             to="home"
             spy={true}
@@ -72,7 +72,7 @@ const Navbar = () => {
         </div>
 
         {/* Theme toggle on the right */}
-        <div className="flex-shrink-0 -mr-2 sm:mr-0">
+        <div className="flex-shrink-0 mr-2 sm:mr-4">
           <button 
             onClick={toggleTheme}
             className="bg-transparent border-none cursor-pointer p-2 text-lg sm:text-xl hover:opacity-80 transition-opacity"
